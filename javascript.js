6b7d15491f8752cb1c5166575d97d33fbdc4044d
mosspaul <gmosspaul18@gmail.com>
2022-09-06 11:22:19 -0500

    Create changing size functionality

--- javascript.js
+++ javascript.js
@@ -6,27 +6,53 @@
 const bod = document.querySelector(".bod");
 const bigDiv = document.createElement("div");
 bigDiv.classList.add("bigDiv");
+
+const slider = document.createElement("input");
+slider.setAttribute("type", "range");
+slider.setAttribute("min", 1);
+slider.setAttribute("max", 100);
+slider.setAttribute("step", 1);
+slider.setAttribute("value", 50);
+
+
+/*slider.addEventListener("input", function(e) {
+    square = parseInt(e.target.value);
+        
+});*/
+
 // This is for making it only work when the mouse is held down
 let mousePressed = false;
-
-for (let i = 0; i < 16; i++) {
+let square = parseInt(slider.value);
+slider.addEventListener("input", function(e) {
+    square = parseInt(e.target.value);
+for (let i = 0; i < square; i++) {
+    console.log(square);
     const midDiv = document.createElement("div");
     midDiv.classList.add("midDiv");
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < square; i++) {
         const lastDiv = document.createElement("div");
         lastDiv.classList.add("lastDiv");
+        lastDiv.setAttribute("style", `height: ${512 / square}px; width: ${512 / square}px`);
         lastDiv.addEventListener("mousedown", function () {
             mousePressed = true;
+        });
+        lastDiv.addEventListener("mouseup", function () {
+            mousePressed = false;
         });
         lastDiv.addEventListener("mouseover", draw);
         midDiv.appendChild(lastDiv);
     }
     bigDiv.appendChild(midDiv);
 }
-bod.appendChild(bigDiv);
+
+
 
 function draw(e) {
     if (mousePressed == true) {
     e.target.style.backgroundColor = "black";
     }
 }
+
+});
+bod.appendChild(bigDiv);
+bod.appendChild(slider);
